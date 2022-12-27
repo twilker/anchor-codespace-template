b986c92e0a7b403f52a7216138075da22cca19d5
Tobias Wilker <twilker@phoenixcontact.com>
2022-12-27 07:47:47 +0100

    Activate cargo after install.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -15,7 +15,8 @@ apt-get install -y \
   libudev-dev
 
 ## Install rustup and common components
-curl https://sh.rustup.rs -sSf | sh -s -- -y 
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source "$HOME/.cargo/env"
 rustup component add rustfmt
 rustup component add clippy
 
