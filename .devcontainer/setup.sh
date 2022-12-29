b2ff61e33ea598d979116fd9a151c99e7c8ecc81
Tobias Wilker <twilker@phoenixcontact.com>
2022-12-29 13:53:55 +0000

    Add wget

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -12,7 +12,8 @@ apt-get install -y \
   openssl \
   libssl-dev \
   pkg-config \
-  libudev-dev
+  libudev-dev \
+  wget
 
 ## Install OpenSSL 1.1 (needed for rust compiler)
 wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb && dpkg -i libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
